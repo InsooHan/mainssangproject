b3e8156276cf37df1d9a145f58f8a331a06593d2
notebook <deerinsect@naver.com>
2022-11-18 16:38:42 +0900

    d

--- MainProject/src/main/java/fpro/data/service/MemberService.java
+++ MainProject/src/main/java/fpro/data/service/MemberService.java
@@ -1,5 +1,41 @@
 package fpro.data.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fpro.data.dto.MemberDto;
+import fpro.data.mapper.MemberMapperInter;
+
+@Service
 public class MemberService implements MemberServiceInter {
+	
+	@Autowired
+	MemberMapperInter mapper;
+
+	@Override
+	public void insertMember(MemberDto dto) {
+		// TODO Auto-generated method stub
+		mapper.insertMember(dto);
+	}
+
+	@Override
+	public String getName(String id) {
+		// TODO Auto-generated method stub
+		return mapper.getName(id);
+	}
+
+	@Override
+	public List<MemberDto> getAllMembers() {
+		// TODO Auto-generated method stub
+		return mapper.getAllMembers();
+	}
+
+	@Override
+	public MemberDto getDataById(String id) {
+		// TODO Auto-generated method stub
+		return mapper.getDataById(id);
+	}
 
 }
--- MainProject/src/main/java/fpro/data/service/MemberServiceInter.java
+++ MainProject/src/main/java/fpro/data/service/MemberServiceInter.java
@@ -1,5 +1,14 @@
 package fpro.data.service;
 
-public interface MemberServiceInter {
+import java.util.List;
 
+import fpro.data.dto.MemberDto;
+
+public interface MemberServiceInter {
+	
+	public void insertMember(MemberDto dto);
+	public String getName(String id);
+	public List<MemberDto> getAllMembers();
+	public MemberDto getDataById(String id);
+	
 }
